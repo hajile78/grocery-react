498fe7778df343f7615eebc1c9731fa322ea9eed
Elijah Winterton <elijah@wintertons.us>
2021-11-05 11:21:00 -0400

    diff form

--- src/App.js
+++ src/App.js
@@ -181,35 +181,22 @@ function App() {
         </div>
       </section>
       <section className="section-center">
-        <form
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          data-netlify-recaptcha="true"
-        >
-          <div className="fields">
-            <div className="field half">
-              <label for="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label for="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field half">
-              <label for="message">Message</label>
-              <textarea name="message" id="message" rows="6"></textarea>
-            </div>
-          </div>
-          <div data-netlify-recaptcha="true"></div>
-          <ul>
-            <li>
-              <input type="submit" value="Send Message" className="primary" />
-            </li>
-            <li>
-              <input type="reset" value="Clear" />
-            </li>
-          </ul>
+        <form name="contact" method="POST" data-netlify="true">
+          <p>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" />
+          </p>
+          <p>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" />
+          </p>
+          <p>
+            <label for="message">Message</label>
+            <textarea id="message" name="message"></textarea>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </section>
     </div>
