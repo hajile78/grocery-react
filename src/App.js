a8cec12028300bcbe9c539939e5279ce6e1436de
Elijah Winterton <elijah@wintertons.us>
2021-11-08 10:04:39 -0500

    netlify from updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import Alert from "./components/Alert";
 import List from "./components/List";
+import Form from "./components/Form";
 import "./App.css";
 
 const getLocalStorage = () => {
@@ -181,24 +182,7 @@ function App() {
         </div>
       </section>
       <section className="section-center">
-        <form name="contact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
-          </p>
-          <p>
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" />
-          </p>
-          <p>
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message"></textarea>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <Form />
       </section>
     </div>
   );
