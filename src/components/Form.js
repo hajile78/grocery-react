d23ac113c79fe403c3a5c9b52131930dcef77c48
Elijah Winterton <elijah@wintertons.us>
2021-11-08 11:39:05 -0500

    fix import of useState and useEffect

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Form() {
   const [success, setSuccess] = useState(false);
@@ -10,31 +10,31 @@ export default function Form() {
   }, []);
 
   return (
-    <>  
-    {success && (<p style={{color: "green"}}>Message has been sent!</p>)}  
-        <form
+    <>
+      {success && <p style={{ color: "green" }}>Message has been sent!</p>}
+      <form
         name="contact"
         method="POST"
         action="/?success=true"
         data-netlify="true"
-        >
+      >
         <input type="hidden" name="form-name" value="contact" />
         <p>
-            <label htmlFor="name">Name</label> <br />
-            <input type="text" id="name" name="name" required />
+          <label htmlFor="name">Name</label> <br />
+          <input type="text" id="name" name="name" required />
         </p>
         <p>
-            <label htmlFor="email">Email</label> <br />
-            <input type="email" id="email" name="email" required />
+          <label htmlFor="email">Email</label> <br />
+          <input type="email" id="email" name="email" required />
         </p>
         <p>
-            <label htmlFor="message">Message</label> <br />
-            <textarea id="message" name="message" required></textarea>
+          <label htmlFor="message">Message</label> <br />
+          <textarea id="message" name="message" required></textarea>
         </p>
         <p>
-            <input type="submit" value="Submit message" />
+          <input type="submit" value="Submit message" />
         </p>
-        </form>
+      </form>
     </>
   );
 }
